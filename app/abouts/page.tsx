aa5fa23e9bb4455669b289545c0afa8883244d48
KAFKHA YASIN A <kafkhayasin@gmail.com>
2026-03-14 20:10:05 +0700

    Prisma first commit

---
+++ app/abouts/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function About() {
+  return (
+    <div>
+      <Navbar />
+      <p>Welcome to our about page!</p>
+      <p>about us</p>
+    </div>
+  );
+}
